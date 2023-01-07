5942bd132aae3412212c3585a43d44037bec24d9
OctavioProgramador <3amsro@gmail.com>
2023-01-06 21:09:08 -0800

    Ejercicio 3

--- ejercicio3/codigos_php.php
+++ ejercicio3/codigos_php.php
@@ -1,7 +1,19 @@
 <?php
 include("bd/abrir_conexion.php");
 	 
-	
+if(isset($_POST['buscar']))	{
+    $doc = $_POST['doc'];
+    $valores = array();
+    $valores['existe'] = "0";
+    //CONSULTAR
+    $resultados = mysqli_query($conexion, "SELECT * FROM $tabla_db1 WHERE doc = '$doc'");
+    while ($consulta = mysqli_fetch_array($resultados)) {
+        $valores['existe'] = "1";
+        $valores['nombre'] = $consulta['nombre'];
+        $valores['direccion'] = $consulta['direccion'];
+        $valores['telefono'] = $consulta['telefono'];
+    }
+    $mi_variable = json_encode($mi_variable);
+    echo $mi_variable;
+}
 include("bd/cerrar_conexion.php");
-?>
-
--- ejercicio3/registro.php
+++ ejercicio3/registro.php
@@ -1,4 +1,5 @@
 <html>
+
 <head>
   <title>Programando Ando</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
@@ -7,63 +8,93 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
-<body>
-<div class="container">
-  <div class="row">
-  <!-- INICIA LA COLUMNA -->
-    <div class="col-md-4 offset-md-4">
-      <center><h1>PROPIETARIO</h1></center>
 
-      <form method="POST" action="registro.php" >
-        <!--Campo Documento-->
-        <div class="mb-3">
-          <label for="doc">Documento</label>
-          <input type="text" name="doc" class="form-control" id="doc">
-        </div>
-        <!--Campo Nombre-->
-        <div class="mb-3">
+<body>
+  <div class="container">
+    <div class="row">
+      <!-- INICIA LA COLUMNA -->
+      <div class="col-md-4 offset-md-4">
+        <center>
+          <h1>PROPIETARIO</h1>
+        </center>
+
+        <form>
+          <!--Campo Documento-->
+          <div class="mb-3">
+            <label for="doc">Documento</label>
+            <input type="text" name="doc" class="form-control" id="doc" onblur="buscar_datos();">
+          </div>
+          <!--Campo Nombre-->
+          <div class="mb-3">
             <label for="nombre">Nombre </label>
             <input type="text" name="nombre" class="form-control" id="nombre">
-        </div>
-        <!--Campo Dirección-->
-        <div class="mb-3">
+          </div>
+          <!--Campo Dirección-->
+          <div class="mb-3">
             <label for="dir">Dirección </label>
             <input type="text" name="dir" class="form-control" id="dir">
-        </div>
-        <!--Campo Teléfono-->
-        <div class="mb-3">
+          </div>
+          <!--Campo Teléfono-->
+          <div class="mb-3">
             <label for="tel">Teléfono </label>
             <input type="text" name="tel" class="form-control" id="tel">
-        </div>
-        <!--Botones-->
-        <center>
-          <input type="submit" value="ENVIAR" class="btn btn-success" name="btn_enviar">
-          <input type="submit" value="CANCELAR" class="btn btn-danger" name="btn_cancelar">
-        </center>
-      </form>
+          </div>
+          <!--Botones-->
+          <center>
+            <input type="button" value="ENVIAR" class="btn btn-success" name="btn_enviar">
+            <input type="button" value="CANCELAR" class="btn btn-danger" name="btn_cancelar">
+          </center>
+        </form>
+      </div>
     </div>
   </div>
-</div>
 </body>
+
 </html>
 
+<script>
+  function buscar_datos() {
+    doc = $("#doc").val();
+
+    var parametros = {
+      "buscar": "1",
+      "doc": doc
+    };
+    $.ajax({
+      data: parametros,
+      dataType: 'json',
+      url: 'codigos_php.php',
+      type: 'post',
+      beforeSend: function() {
+        alert("enviando");
+      },
+      error: function() {
+        alert("Error");
+      },
+      complete: function() {
+        alert("¡Listo!");
+      },
+      success: function(valores) {
+      }
+    })
+  }
+</script>
+
 <?php
-    include("bd/abrir_conexion.php");
-      
-      $doc    ="";
-      $nombre ="";
-      $dir    ="";
-      $tel    ="";
+include("bd/abrir_conexion.php");
 
-      if(isset($_POST['btn_enviar']))
-      {      
-        echo "Presiono el boton Enviar";
-      }
+$doc    = "";
+$nombre = "";
+$dir    = "";
+$tel    = "";
 
-      if(isset($_POST['btn_cancelar']))
-      {
-        echo "Presiono el boton Cancelar";
-      }
+if (isset($_POST['btn_enviar'])) {
+  echo "Presiono el boton Enviar";
+}
 
-    include("bd/cerrar_conexion.php");
-  ?>
+if (isset($_POST['btn_cancelar'])) {
+  echo "Presiono el boton Cancelar";
+}
+
+include("bd/cerrar_conexion.php");
+?>
